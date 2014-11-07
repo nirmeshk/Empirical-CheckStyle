5c7bfd6e5a27d9f5edb096769b95117674913bfe
Nirmesh <nbkhande@ncsu.edu>
2014-11-07 01:07:16 -0500

    Added methods to write database and renamed basic_style_check_on_project.py

--- style_check_project.py
+++ style_check_project.py
@@ -5,9 +5,9 @@ Reference: I got motivation for this script from https://gist.github.com/gregory
 import os
 import re
 import sys
-import basic_style_check_on_project
+import MySQLdb
 
-def main():  
+def main():
     if(len(sys.argv) < 2):
         print("Please provide python file or package path as command line argument")
         exit()
@@ -21,10 +21,9 @@ def main():
     else:
         print("Provided input is neither python file nor a python package."
               " Please Provide a valid python file path or python package path")
-        exit()        
+        exit()    
 
-def check(module):
-    global total, count
+def check(module_name, module_path, write_to_db = False, commit_hash='-'):
     print("CHECKING ", module)
     messages = {}
     pout = os.popen('pylint -r y %s'% module, 'r')
@@ -47,9 +46,30 @@ def check(module):
     print("=="*50)
     print("number_of_statements: " + str(number_of_statements) ) 
     print("score: " + str(score) )
-  
+
+    if write_to_db:
+        write_db(module_name, messages, number_of_statements, score, commit_hash)
+
+
+def write_db(project, 
+            messages_dict, 
+            number_of_statements, 
+            score, 
+            commit_hash):
+    db = MySQLdb.connect("localhost","root","root","checkstyle" )
+    cursor = db.cursor()
+    query1 = "INSERT INTO code_analysis (repository, commit_hash, message, message_count) VALUES(%s, %s, %s, %d)"
+    query2 = "INSERT INTO code_score (project, commit_hash, statement_count, score) VALUES(%s,%s, %d, %f)"
+
+    for key in messages_dict:
+        cursor.execute( query1, (project, commit_hash, key, messages_dict[key]) )
+        db.commit()
+
+    cursor.execute( query2, (project, commit_hash, number_of_statements, score) )
+    db.commit()
+
+    db.close()
+
 if __name__ == "__main__":
-    main()    
-    #print "==" * 50
-    #print "%d modules found"% count
-    #print "AVERAGE SCORE = %.02f"% (total / count)
+    main()
+
