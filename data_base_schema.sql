dffd346437053559edfef439a2e4356e9bd5cf11
Nirmesh <nbkhande@ncsu.edu>
2014-11-13 05:40:39 -0500

    updating the table schema to add some fields

--- data_base_schema.sql
+++ data_base_schema.sql
@@ -1,14 +1,18 @@
 CREATE TABLE `code_analysis` (
   `project` varchar(255) DEFAULT NULL,
   `commit_hash` varchar(255) DEFAULT NULL,
+  `commit_number` int(11) DEFAULT NULL,
   `message` varchar(255) DEFAULT NULL,
   `message_count` int(11) DEFAULT NULL,
-  `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `project` (`project`),
+  KEY `commit_hash` (`commit_hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 CREATE TABLE `code_score` (
   `project` varchar(255) DEFAULT NULL,
   `commit_hash` varchar(255) DEFAULT NULL,
+  `commit_number` int(11) DEFAULT NULL,
   `statement_count` int(11) DEFAULT NULL,
   `score` float(4,2) DEFAULT NULL,
   `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
